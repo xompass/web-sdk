8daf68549407d59dd622d36f634e9ef493f496d3
Rene Mena <rene.mena.m@gmail.com>
2025-08-04 22:40:59 -0400

    face recognition sensor

---
+++ src/models/sensorTypes/FaceRecognition/FaceRecognitionContent.ts
@@ -0,0 +1,33 @@
+export type FaceRecognitionContent = {
+  files?: {
+    image?: {
+      container?: string;
+      name?: string;
+      type?: 'image/jpg' | 'image/jpeg' | 'image/png';
+      field?: string;
+      originalFilename?: string;
+      meta?: Record<string, any>;
+      datasourceName?: string;
+    }[];
+    faces?: {
+      container?: string;
+      name?: string;
+      type?: 'image/jpg' | 'image/jpeg' | 'image/png';
+      field?: string;
+      originalFilename?: string;
+      meta?: Record<string, any>;
+      datasourceName?: string;
+    }[];
+  };
+  faces?: {
+    subjectId?: string;
+    subject: string;
+    probability: number;
+    frame: {
+      x: number;
+      y: number;
+      w: number;
+      h: number;
+    };
+  }[];
+};
---
+++ src/models/sensorTypes/FaceRecognition/FaceRecognitionTRCParameters.ts
@@ -0,0 +1,5 @@
+export type FaceRecognitionBasedOnRulesTRCParameters = {
+  thresholds?: {
+    minProbability?: number;
+  };
+};
